51cff03d96e73c8225a0b85a7950d0323b444251
Xavier Garrido <xavier.garrido@gmail.com>
2016-05-11 18:45:29 +0200

    change other colors

--- themes/mookid/gtk-3.0/change_color.sh
+++ themes/mookid/gtk-3.0/change_color.sh
@@ -1,6 +1,6 @@
 sed \
     -e 's/#e5e5e5/#ecedee/' \
-    -e 's/#546e7a/#fec006/' \
-    -e 's/#275f79/#f34235/' \
+    -e 's/#546e7a/#268BD2/' \
+    -e 's/#275f79/#268BD2/' \
+    -e 's/#347D9F/#268BD2/' \
     gtk.css.orig > gtk.css
-    # -e 's/#f7f7f7/#fec006/' \
